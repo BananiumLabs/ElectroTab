6d003ec8e06e6c4c1954e6740e9ba87d3eda11e8
Ben Cuan <ben.cuan@gmail.com>
2017-09-04 13:58:00 -0700

    Remove fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent {
 
     logout() {
         this.authService.logout();
-        this.router.navigate(['/']);
+        this.router.navigate(['/info']);
     }
 
     onResetPasswordSuccess() {
