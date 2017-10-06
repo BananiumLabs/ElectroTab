ba33dd0653901b47b89d739ccea400ce127bd147
Ben Cuan <ben.cuan@gmail.com>
2017-10-06 16:33:01 -0700

    Remove grid-content.component and grid-menu.component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardPageComponent } from 'app/home/dashboard-page/dashboard-page.component';
 import { GridsterModule } from 'angular-gridster2';
-import { GridContentComponent } from 'app/grid/grid-content.component';
 import { WidgetService } from 'app/grid/widget.service';
 import { GridService } from 'app/grid/grid.service';
 import { CompileService } from 'app/shared/compile/compile.service';
@@ -11,8 +10,7 @@ import { CompileModule } from 'app/shared/compile/compile.module';
 
 @NgModule({
     declarations: [
-        DashboardPageComponent,
-        GridContentComponent
+        DashboardPageComponent
     ],
     imports: [
         CommonModule,
@@ -29,8 +27,7 @@ import { CompileModule } from 'app/shared/compile/compile.module';
         }),
     ],
     exports: [
-        RouterModule,
-        GridContentComponent
+        RouterModule
     ],
     providers: [
         WidgetService,
